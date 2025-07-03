0ec3a2193314ba3af4722d7ce0d78a2ca4832352
Pratik Vadaviya <pratikpatel9681@gmail.com>
2025-07-04 00:52:22 +0530

    Complete WelcomePage and starting authentication part , add photos

--- Frontend/MyWalletFrontend/src/App.jsx
+++ Frontend/MyWalletFrontend/src/App.jsx
@@ -1,16 +1,51 @@
-import { useState } from 'react'
+// import './assets/styles/index.css';
 
-
+const Welcome = lazy(() => import('./pages/welcome.jsx'))
+const NotFoundPage = lazy(() => import('./pages/auth/notFound'))
+const Login = lazy(() => import('./pages/auth/login/login.jsx'))
+const Register = lazy(() => import('./pages/auth/register/register.jsx'))
+const UnAuthorizedAccessPage = lazy(() => import('./pages/auth/unAuthorized.jsx'))
 
 function App() {
  
   return (
     <>
-      <p >
-        Click on the Vite and React logos to learn more
-      </p>
+       <Suspense fallback={<LoadingSpinner />}>
+            <ThemeContext.Provider value={{ isDarkMode, toggleTheme }}>
+                <RoutesWrapper isDarkMode={isDarkMode}>
+                    <Routes>
+
+                        <Route path="/" element={<Welcome />} />
+                        <Route path="/auth/login" element={<Login />} />
+                        <Route path="/auth/register" element={<Register />} />
+                        <Route path="/unauthorized" element={<UnAuthorizedAccessPage />} />
+                        <Route path="*" element={<NotFoundPage />} />
+
+                      </Routes>
+                </RoutesWrapper>
+            </ThemeContext.Provider>
+        </Suspense>
     </>
   )
+}
+
+
+
+function RoutesWrapper({ children, isDarkMode }) {
+    return (
+        <div className={isDarkMode ? "dark" : "light"}>
+            {children}
+        </div>
+    )
+
+}
+
+function LoadingSpinner() {
+    return (
+        <div style={{ width: '100%', height: '100vh' }}>
+            <Loading />
+        </div>
+    )
 }
 
 export default App
