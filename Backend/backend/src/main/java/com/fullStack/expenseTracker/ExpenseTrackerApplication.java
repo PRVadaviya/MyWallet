b4cddeb9c3f8f7c9badc4a1528cb44954abb071c
Pratik Vadaviya <pratikpatel9681@gmail.com>
2025-07-12 23:43:34 +0530

    -Add user dashboard and profile management features. - Implement sidebar navigation with role-based links for users and admins. - Introduce admin user management

--- Backend/backend/src/main/java/com/fullStack/expenseTracker/ExpenseTrackerApplication.java
+++ Backend/backend/src/main/java/com/fullStack/expenseTracker/ExpenseTrackerApplication.java
@@ -1,14 +1,13 @@
-package com.mywallet.backend;
+package com.fullStack.expenseTracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BackendApplication {
+public class ExpenseTrackerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
+		SpringApplication.run(ExpenseTrackerApplication.class, args);
 	}
 
 }
-	
